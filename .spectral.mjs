12cf1fd7df023c069055816fad28aae80e54bd8c
afeayos@gmail.com <afeayos@gmail.com>
2024-08-16 10:41:04 +0100

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI1NTgyNA/spectral.js?branch_slug=master&token=b4855e50-e574-44e4-955f-8e972fa2fa4d";
+export default { extends: ruleset };
